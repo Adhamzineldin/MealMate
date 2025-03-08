96bed9454d9625ff2949ca705fd98dd302880dc3
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-03-08 14:23:26 +0200

    Refactor: Enhance Ingredient Details UI and Logic
    -   Removed `MealDetailsFragment` and `MealDetailsViewModel`.
    -   Updated `IngredientsAdapter` to fetch and display ingredient icons from `themealdb.com` using `Glide`.
    -   Added circular image transformation for ingredient icons.
    -   Updated `item_ingredient_details.xml` to show the ingredient icon as the main image instead of the placeholder background.
    -   Updated the ingredient view to show a correct image and remove the gray background.

--- app/src/main/java/com/maayn/mealmate/presentation/details/adapters/IngredientsAdapter.kt
+++ app/src/main/java/com/maayn/mealmate/presentation/details/adapters/IngredientsAdapter.kt
@@ -3,6 +3,9 @@ package com.maayn.mealmate.presentation.details.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.maayn.mealmate.R
 import com.maayn.mealmate.data.local.entities.IngredientEntity
 import com.maayn.mealmate.databinding.ItemIngredientDetailsBinding
 
@@ -28,6 +31,16 @@ class IngredientsAdapter(private val ingredients: List<IngredientEntity>) :
         fun bind(ingredient: IngredientEntity) {
             binding.tvIngredientName.text = ingredient.name
             binding.tvIngredientMeasure.text = ingredient.measure
+
+            val formattedIngredient = ingredient.name.replace(" ", "%20")
+            val imageUrl = "https://www.themealdb.com/images/ingredients/$formattedIngredient.png"
+
+            Glide.with(binding.root.context)
+                .load(imageUrl)
+                .transform(CircleCrop())  // Make image circular
+                .placeholder(R.drawable.ingredient)
+                .error(R.drawable.ingredient)
+                .into(binding.ivIngredientIcon)
         }
     }
 }
